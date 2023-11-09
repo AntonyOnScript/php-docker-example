23e50d581955e8d336c50a9521458243a000cabc
AntonyOnScript <santhiagoafk@gmail.com>
2023-11-09 12:49:28 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+PHP + Docker example, just run `docker-compose up` and you'll get it running on your http://localhost:8082/
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+$test = $_GET;
+
+if (count($test) > 0) {
+  echo var_dump($test);
+}
+
+echo 'i Love PHP <3';
+
+?>
